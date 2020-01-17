59c1a09aa999cfaeef4a76645d9a063fac59a74b
Romamart <martinyuk.r@yandex.ru>
2020-01-17 21:07:30 +0300

    Task №8

---
+++ Task_8/Task_8.2/cmake-build-debug/CMakeFiles/Task_8_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Task_8_1.dir/src/main/main.cpp.o"
+  "Task_8_1"
+  "Task_8_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Task_8_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Task_8/Task_8.2/cmake-build-debug/CMakeFiles/Task_8_2.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Task_8_2.
+# This may be replaced when dependencies are built.
